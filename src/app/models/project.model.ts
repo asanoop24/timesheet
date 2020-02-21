dfa07e5bec7ca449789711c502c331aee286dbba
Anoop Sharma <anoop.sharma1@firstsource.com>
2020-02-21 23:14:06 +0530

    projects-WIP

--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -1,4 +1,7 @@
 export interface Project {
   project_id: number;
   project_name: string;
+  project_code: string;
+  project_type: string;
+  project_owner: string;
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 import { DataService } from './../services/data.service';
 import { Project } from './../models/project.model';
 import { Observable } from 'rxjs/Observable';
@@ -9,12 +10,17 @@ import * as _ from 'lodash';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent implements OnInit {
 
   allProjects: Project[] = [];
   newProjects: Project[] = [];
+  newProjectFlag: number = 0;
+  activeProjectFlag: boolean = false;
+  activeProject: Project;
+
+  newProjectForm;
   constructor(private dataService: DataService) { }
 
 
@@ -24,19 +30,45 @@ export class ProjectsComponent implements OnInit {
       data => {if(data['data']) this.allProjects = data['data']},
       error => console.log(error)
     );
+    this.newProjectForm = new FormGroup({
+      'name': new FormControl(),
+      'code': new FormControl(),
+      'owner': new FormControl(),
+      'type': new FormControl()
+    });
+  }
+
+  ngOnDestroy(){
+    // alert('Are you sure?');
   }
 
   addNewProject(){
+    this.newProjectFlag=1;
+    console.log(this.allProjects);
+  }
+  cancelNewProject(){
+    this.newProjectFlag = 0;
+  }
+  onSubmit(){
+    const name = this.newProjectForm.value.name;
+    const code = this.newProjectForm.value.code;
+    const owner = this.newProjectForm.value.owner;
+    const type = this.newProjectForm.value.type;
+
     let project: Project = {
       'project_id': this.allProjects && this.allProjects.length ? Math.max.apply(null, (this.allProjects.map(project => project.project_id))) + 1 : 1,
-      'project_name': ''
+      'project_name': name,
+      'project_code': code,
+      'project_type': type,
+      'project_owner': owner,
     }
     this.allProjects.push(project);
-  }
 
-  onSubmit(){
-    console.log(this.allProjects);
     this.dataService.updateProjects(this.allProjects);
   }
-
+  
+  selectProject(project){
+    this.activeProject = this.activeProject ? undefined : project;
+    this.activeProjectFlag = true;
+  }
 }
